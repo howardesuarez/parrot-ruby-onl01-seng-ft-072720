70516f7660aca629a6c5175c837c0acd2d036005
howard suarez <howardesuarez@gmail.com>
2020-07-28 22:12:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def #parrot(sound = "Squawk!")
-  puts "Hello, #{sound}"
-  "Hello, #{sound}"
+def parrot("Squawk!")
+  puts ""
 end
